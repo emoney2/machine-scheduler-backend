11f0bf73b13df979bb8fc9822087d19b6d501d76
Justin Eckard <eckardjustin@gmail.com>
2025-07-02 15:14:37 -0400

    🧵 Fix: copy .emb files only on reorder and pass correct folder ID

--- server.py
+++ server.py
@@ -916,7 +916,6 @@ def submit_order():
 
         # 1) Make the root folder for this order
         order_folder_id = create_folder(new_order, parent_id="1n6RX0SumEipD5Nb3pUIgO5OtQFfyQXYz")
-        copy_emb_files(data.get("reorderFrom", ""), new_order, drive, "1n6RX0SumEipD5Nb3pUIgO5OtQFfyQXYz")
         make_public(order_folder_id)
 
         # 🧵 If this is a reorder, copy any .emb files from the original job folder
